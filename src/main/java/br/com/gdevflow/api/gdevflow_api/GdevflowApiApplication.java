0972ec490a017a910b96717bf5e1a288e003d492
Thibor Martin <thibormartin28@outlook.com>
2026-02-27 13:12:45 -0300

    initial commit

---
+++ src/main/java/br/com/gdevflow/api/gdevflow_api/GdevflowApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gdevflow.api.gdevflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdevflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdevflowApiApplication.class, args);
+	}
+
+}
